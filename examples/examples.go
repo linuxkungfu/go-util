552fd71a32b69f328f7d0bfd0fd0071c1aa41e9f
jianjunzhou <jianjun.zhou@ctechm.com>
2023-12-26 19:05:38 +0800

    add countires modules

--- examples/examples.go
+++ examples/examples.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 
 	"github.com/linuxkungfu/go-util"
+	"github.com/linuxkungfu/go-util/dep/countries"
 )
 
 func main() {
 	serverId := util.CreateServerId("123")
 	fmt.Printf("server id:%s\n", serverId)
+	emoji, unicode := util.GetFlag("US")
+	fmt.Printf("emoji:%s unicode:%s\n", emoji, unicode)
+	fmt.Printf("country name:%s\n", countries.ByName("US").String())
+
 }
